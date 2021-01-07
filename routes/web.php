7c60d42701462a676ae0991099108b0fd2d1f49a
3a732037 <3a732037@gm.student.ncut.edu.tw>
2021-01-07 16:30:44 +0800

    練習3.3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home',function(){
-    return view('home.index');
-});
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('hello/{name?}',[HelloController::class,'index'])
+    ->name('hello.index');
